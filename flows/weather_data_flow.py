60b7b171d8f9b494efba28afa5641dc37572963e
Medun <donhuanmatus62@gmail.com>
2025-12-07 00:00:37 +0200

    Improve taks and flow, clean errors

--- flows/weather_data_flow.py
+++ flows/weather_data_flow.py
@@ -4,27 +4,25 @@ from prefect import flow, task
 from extract.extract_from_open_weather import extract_historical_data_for_new_york
 
 
-@task
-def extract_weather_data(cities_name:str,country_code:str, start:str, end:str, api_key:str):
-    weather_data = extract_historical_data_for_new_york(cities_name,country_code, start, end, api_key)
+@task(name="My Extract Task",
+      description="An extract task for the weather flow.",
+      task_run_name="data extraction")
+def extract_weather_data(city_name,country_code, start, end, api_key):
+    weather_data = extract_historical_data_for_new_york(city_name,country_code, start, end, api_key)
     return weather_data
 
 
 
-@flow
-def weather_flow_parallel(cities_names, country_code, start, end, api_key):
+@flow(flow_run_name="weather_flow_runs")
+def weather_flow(city_name, country_code, start, end, api_key):
     # Run extraction in parallel for all cities using .map()
-    results = extract_weather_data.map(
-        city_name=cities_names,
-        country_code=[country_code]*len(cities),
-        start=[start]*len(cities),
-        end=[end]*len(cities),
-        api_key=[api_key]*len(cities)
-    )
-    return results
+    data = extract_weather_data(city_name, country_code, start, end, api_key)
+
+
+
 
 
 # Example usage
 if __name__ == "__main__":
-    cities = ["New York", "Boston", "Chicago"]
-    weather_data = weather_flow_parallel(cities, "US", "01-01-2001", "02-01-2001", config("API_KEY")
+
+    weather_flow("New Work", "US", "01-01-2001", "02-01-2001", config("OPEN_WEATHER_API_KEY"))
