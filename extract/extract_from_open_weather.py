b6baa0c082923e370f831c2554539126b04202e8
Medun <donhuanmatus62@gmail.com>
2025-12-07 00:00:37 +0200

    Improve taks and flow, clean errors

--- extract/extract_from_open_weather.py
+++ extract/extract_from_open_weather.py
@@ -2,7 +2,7 @@ import requests
 
 
 
-def extract_historical_data_for_new_york(city_name:str,country_code:str, start:str, end:str, api_key:str) -> json:
+def extract_historical_data_for_new_york(city_name:str,country_code:str, start:str, end:str, api_key:str):
     url = (f"https://history.openweathermap.org/data/2.5/history/city?q={city_name},"
            f"{country_code}&type=hour&start={start}&end={end}&appid={api_key}")
 
